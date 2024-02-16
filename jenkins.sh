4513144d4e02740d46727a23d80b3ec96acb812f
balkhair786 <76272905+balkhair786@users.noreply.github.com>
2024-02-16 09:04:54 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -11,8 +11,6 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
-sudo apt-get docker.io -y
-service docker start
 sudo apt-get install jenkins -y
 service jenkins start
 cat /var/lib/jenkins/secrets/initialAdminPassword
